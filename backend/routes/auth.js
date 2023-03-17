46625cb515416d2da84f66ff47be343d82aa380b
Hinesh Kumar <hineshkumar46@gmail.com>
2023-03-17 18:22:17 +0500

    Complete Video 50

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -7,7 +7,7 @@ var jwt = require('jsonwebtoken');
 
 const JWT_SECRET = 'HineshIsAGoodBoy';
 
-//create a User using: post 'api/auth/createuser' : no Login Required 
+//Route 1 :create a User using: post 'api/auth/createuser' : no Login Required 
 router.post('/createuser',[
   //Valitidion for Fields through Express Validtion
      body('name','Enter a Valid Name').isLength({min:3}),
@@ -49,4 +49,42 @@ router.post('/createuser',[
     res.status(500).send("Some Error Occured");
   }
 })
+
+//Route 2: Authenticate a User using: POST 'api/auth/login' : no Login Required 
+router.post('/login',[
+  //Valitidion for Fields through Express Validtion
+     body('email','Enter a Valid Email').isEmail(),
+     body('password','Password cannot be Blank').exists()
+],async (req,res)=>{
+  //If there are Error Return Bad Request and the errors
+  const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const {email,password} = req.body;
+    try {
+      let user = await User.findOne({email});
+      if(!user){
+        res.status(400).json({error: "Please try to Login with correct Credentials"})
+      }
+      //Comapre my User Password with Database password
+      let CompareUserPassword = await bcrypt.compare(password,user.password);
+      if(!CompareUserPassword){
+        res.status(400).json({error: "Please try to Login with correct Credentials"})
+      }
+      const data = {
+        user:{
+          id : user.id,
+        }
+       }
+       const authToken =  jwt.sign(data,JWT_SECRET);
+       res.json(authToken)
+    } catch (error) {
+       console.error(error.message);
+      res.status(500).send("Internal Some Error");
+    }
+})
+
+//Route 2: Authenticate a User using: POST 'api/auth/login' : no Login Required 
+
 module.exports = router
