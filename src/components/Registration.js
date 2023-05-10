1e43310c60c22b946316e60cb7edb5c6ef436ff6
Hinesh Kumar <hineshkumar46@gmail.com>
2023-05-10 15:31:55 +0500

    complete video 69

---
+++ src/components/Registration.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
+function Registration() {
+  return (
+    <div>This Is Registration</div>
+  )
+}
+
+export default Registration
